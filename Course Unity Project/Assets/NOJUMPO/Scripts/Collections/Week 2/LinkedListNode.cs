e74ef206d03802cb38c04564bf8dd4d7f7cd5704
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-27 07:43:33 +0300

    Week 2: LinkedListNode.cs Created

---
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/LinkedListNode.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Nojumpo
+{
+    [Serializable]
+    public class LinkedListNode
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ----------------------------- CONSTRUCTORS ------------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
