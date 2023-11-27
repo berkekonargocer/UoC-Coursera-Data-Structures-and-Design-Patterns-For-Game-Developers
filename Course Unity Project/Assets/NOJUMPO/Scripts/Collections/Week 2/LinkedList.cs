78586e046746e2864eb4655b0b314aaba3b232bd
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-27 08:03:43 +0300

    Week 2: LinkedList.cs Fields and Constructor Created | Clear Method Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/LinkedList.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/LinkedList.cs
@@ -1,15 +1,41 @@
 namespace Nojumpo.Collections
 {
-    public class LinkedList<T>
+    public abstract class LinkedList<T>
     {
         // -------------------------------- FIELDS ---------------------------------
+        public LinkedListNode<T> Head { get { return _head; } }
+        public int Count { get { return _count; } }
 
+        protected LinkedListNode<T> _head;
+        protected int _count;
 
         // ----------------------------- CONSTRUCTORS ------------------------------
-
+        protected LinkedList() {
+            _head = null;
+            _count = 0;
+        }
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public abstract void Add(T item);
 
+        public void Clear() {
+            if (_head != null)
+            {
+                LinkedListNode<T> previousNode = _head;
+                LinkedListNode<T> currentNode = _head.Next;
+                previousNode.Next = null;
+
+                while (currentNode != null)
+                {
+                    previousNode = currentNode;
+                    currentNode = currentNode.Next;
+                    previousNode.Next = null;
+                }
+
+                _head = null;
+                _count = 0;
+            }
+        }
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
 
