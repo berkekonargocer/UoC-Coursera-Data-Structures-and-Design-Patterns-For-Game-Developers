94ffda094af2fdcefd8a9d10a8b6d94bdcce38fc
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-28 11:29:25 +0300

    Week 2: SortedLinkedList.cs Created

---
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/SortedLinkedList.cs
@@ -0,0 +1,22 @@
+namespace Nojumpo.Collections
+{
+    public class SortedLinkedList<T> : LinkedList<T>
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ----------------------------- CONSTRUCTORS ------------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public override void Add(T item) {
+            throw new System.NotImplementedException();
+        }
+        
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
