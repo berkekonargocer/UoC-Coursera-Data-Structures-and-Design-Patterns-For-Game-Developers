cd47b2ee94d1b0418e63dd44b1da74eeec06747b
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-28 11:02:28 +0300

    Week 2: UnsortedLinkedList.cs Add Method Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/UnsortedLinkedList.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/UnsortedLinkedList.cs
@@ -1,9 +1,6 @@
-using System;
-
-namespace Nojumpo
+namespace Nojumpo.Collections
 {
-    [Serializable]
-    public class UnsortedLinkedList
+    public class UnsortedLinkedList<T> : LinkedList<T>
     {
         // -------------------------------- FIELDS ---------------------------------
 
@@ -12,7 +9,18 @@ namespace Nojumpo
 
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public override void Add(T item) {
+            if (_head == null)
+            {
+                _head = new LinkedListNode<T>(item, null);
+            }
+            else
+            {
+                _head = new LinkedListNode<T>(item, _head);
+            }
 
+            _count++;
+        }
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
 
