a0c9e0f616b5a494a27aa90cf8c56a7cc28478ff
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-29 14:42:52 +0300

    Week 2: GraphNode.cs Fields Created

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/Graph/GraphNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/Graph/GraphNode.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+
 namespace Nojumpo.Collections
 {
     public class GraphNode<T>
     {
         // -------------------------------- FIELDS ---------------------------------
-
+        T value;
+        List<GraphNode<T>> neighbors;
 
         // ----------------------------- CONSTRUCTORS ------------------------------
 
