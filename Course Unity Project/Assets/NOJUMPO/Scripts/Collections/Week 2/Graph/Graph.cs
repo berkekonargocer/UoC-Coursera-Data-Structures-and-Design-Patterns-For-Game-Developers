26c33f2f6419de9309b576e7d235a47b9f917871
Berke Konargöçer <berke.konargocer@gmail.com>
2023-11-30 16:05:18 +0300

    Week 2: Graph.cs Clear Method Created and Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/Graph/Graph.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 2/Graph/Graph.cs
@@ -15,5 +15,18 @@ namespace Nojumpo.Collections
         public Graph() {
                 
         }
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public void Clear() {
+            foreach (GraphNode<T> graphNode in nodes)
+            {
+                graphNode.RemoveAllNeighbors();
+            }
+
+            for (int i = nodes.Count; i > 0; i--)
+            {
+                nodes.RemoveAt(i);
+            }
+        }
     }
 }
