c226f56305eb9bf2e2f95d71bec526d75621e1fd
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-05 16:05:09 +0300

    Take The Short Way Home GraphBuilder.cs Awake Method Implemented

--- Week 2 Assignment/TakeTheShortWayHome/Assets/Scripts/GraphBuilder.cs
+++ Week 2 Assignment/TakeTheShortWayHome/Assets/Scripts/GraphBuilder.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 /// </summary>
 public class GraphBuilder : MonoBehaviour
 {
-    static Graph<Waypoint> graph;
+    static Graph<Waypoint> graph = new Graph<Waypoint>();
 
     #region Constructor
 
@@ -34,13 +34,33 @@ public class GraphBuilder : MonoBehaviour
     {
         // add nodes (all waypoints, including start and end) to graph
         GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Waypoint");
+        Waypoint start = GameObject.FindWithTag("Start").GetComponent<Waypoint>();
+        Waypoint finish = GameObject.FindWithTag("Finish").GetComponent<Waypoint>();
 
+        
+        graph.AddNode(start);
+        graph.AddNode(finish);
         foreach (GameObject gObject in gameObjects)
         {
              graph.AddNode(gObject.GetComponent<Waypoint>());
         }
 
         // add neighbors for each node in graph
+        foreach (GraphNode<Waypoint> node1 in graph.Nodes)
+        {
+            foreach (GraphNode<Waypoint> node2 in graph.Nodes)
+            {
+                if (node1 != node2)
+                {
+                    Vector2 distance = node1.Value.Position - node2.Value.Position;
+
+                    if (Mathf.Abs(distance.x) < 3.5f && Mathf.Abs(distance.y) < 3.0f)
+                    {
+                        node1.AddNeighbor(node2, distance.magnitude);
+                    }
+                }
+            }
+        }
     }
 
     /// <summary>
