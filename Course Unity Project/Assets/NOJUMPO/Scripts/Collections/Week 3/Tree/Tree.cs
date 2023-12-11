a283a98a3666fa788ca53bf83edd5942d6d91d62
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-12 01:40:04 +0300

    Week 3: Tree.cs Fields and Properties Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/Tree.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/Tree.cs
@@ -1,11 +1,15 @@
-using System;
+using System.Collections.Generic;
 
 namespace Nojumpo.Collections
 {
     public class Tree<T>
     {
         // -------------------------------- FIELDS ---------------------------------
-        
+        public TreeNode<T> Root { get { return _root; } }
+        public int Count { get { return _nodes.Count; } }
+
+        TreeNode<T> _root = null;
+        List<TreeNode<T>> _nodes = new List<TreeNode<T>>();
 
         // ----------------------------- CONSTRUCTORS ------------------------------
 
