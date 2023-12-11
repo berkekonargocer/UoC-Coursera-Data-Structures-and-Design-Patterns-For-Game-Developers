65c0cc7041879c712ce235a3f915b237db00dd43
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-11 19:44:25 +0300

    Week 3: TreeNode.cs Fields Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/TreeNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/TreeNode.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
+
 namespace Nojumpo.Collections
 {
     public class TreeNode<T>
     {
         // -------------------------------- FIELDS ---------------------------------
-
+        T _value;
+        TreeNode<T> _parent;
+        List<TreeNode<T>> _children;
 
         // ----------------------------- CONSTRUCTORS ------------------------------
 
