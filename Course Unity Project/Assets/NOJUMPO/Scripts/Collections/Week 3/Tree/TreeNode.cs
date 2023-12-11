1b993ba723ec5929dfb2127e4e7a292679a1f83b
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-11 19:46:53 +0300

    Week 3: TreeNode.cs Properties Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/TreeNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/TreeNode.cs
@@ -5,10 +5,15 @@ namespace Nojumpo.Collections
     public class TreeNode<T>
     {
         // -------------------------------- FIELDS ---------------------------------
+        public T Value { get { return _value; } }
+        public TreeNode<T> Parent { get { return _parent; } set { _parent = value; } }
+        public IList<TreeNode<T>> Children { get { return _children.AsReadOnly(); } }
+
         T _value;
         TreeNode<T> _parent;
         List<TreeNode<T>> _children;
 
+        
         // ----------------------------- CONSTRUCTORS ------------------------------
 
 
