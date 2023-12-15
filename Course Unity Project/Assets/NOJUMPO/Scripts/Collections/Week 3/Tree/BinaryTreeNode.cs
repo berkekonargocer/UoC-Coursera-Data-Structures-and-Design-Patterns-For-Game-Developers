8ecee235cd1612eb47a2535979c453efbc085cda
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-15 19:24:40 +0300

    Week 3: BinaryTreeNode.cs Setup

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
@@ -1,15 +1,20 @@
-using System;
-
-namespace Nojumpo
+namespace Nojumpo.Collections
 {
-    [Serializable]
-    public class BinaryTreeNode
+    public class BinaryTreeNode<T> : TreeNode<T>
     {
         // -------------------------------- FIELDS ---------------------------------
+        TreeNode<T> leftChild;
+        TreeNode<T> rightChild;
 
+        public TreeNode<T> LeftChild { get { return leftChild; } }
+        public TreeNode<T> RightChild { get { return rightChild; } }
 
+        
         // ----------------------------- CONSTRUCTORS ------------------------------
-
+        public BinaryTreeNode(T value, TreeNode<T> parent) : base(value, parent) {
+            leftChild = null;
+            rightChild = null;
+        }
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
 
