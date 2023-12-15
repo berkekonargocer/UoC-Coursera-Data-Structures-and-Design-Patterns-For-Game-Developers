13f4201d10f7f66486a6ac90aff75653f4688d69
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-15 19:12:34 +0300

    Week 3: BinaryTreeNode.cs Created

---
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Nojumpo
+{
+    [Serializable]
+    public class BinaryTreeNode
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ----------------------------- CONSTRUCTORS ------------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
