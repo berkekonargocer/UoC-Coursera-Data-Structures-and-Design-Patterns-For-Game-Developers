11f94b0584c2ab3e8f7caaaf145ece62001383ca
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-16 14:00:44 +0300

    Week 3: BinaryTree.cs AddNode Method Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTree.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTree.cs
@@ -18,7 +18,22 @@ namespace Nojumpo.Collections
             _nodes.Add(_root);
         }
 
+        
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+        public bool AddNode(BinaryTreeNode<T> nodeToAdd, ChildSide childSide) {
+            if (nodeToAdd?.Parent == null || !_nodes.Contains(nodeToAdd.Parent))
+            {
+                return false;
+            }
+
+            if (nodeToAdd.Parent.LeftChild == nodeToAdd || nodeToAdd.Parent.RightChild == nodeToAdd)
+            {
+                return false;
+            }
+            
+            _nodes.Add(nodeToAdd);
+            return nodeToAdd.Parent.AddChild(nodeToAdd, childSide);
+        }
         
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
