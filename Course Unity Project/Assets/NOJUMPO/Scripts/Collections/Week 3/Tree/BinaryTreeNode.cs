7688bb3b8ed486363bbf6f9cde6985a6ddc5476e
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-16 13:32:13 +0300

    Week 3: BinaryTreeNode.cs AddChild Method Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
@@ -7,14 +7,14 @@ namespace Nojumpo.Collections
         // -------------------------------- FIELDS ---------------------------------
         public T Value { get { return _value; } }
         public BinaryTreeNode<T> Parent { get { return _parent; } set { _parent = value; } }
-        public BinaryTreeNode<T> LeftChild { get { return leftChild; } }
-        public BinaryTreeNode<T> RightChild { get { return rightChild; } }
+        public BinaryTreeNode<T> LeftChild { get { return _leftChild; } }
+        public BinaryTreeNode<T> RightChild { get { return _rightChild; } }
         public IList<BinaryTreeNode<T>> Children { get { return _children.AsReadOnly(); } }
 
         T _value;
         BinaryTreeNode<T> _parent;
-        BinaryTreeNode<T> leftChild;
-        BinaryTreeNode<T> rightChild;
+        BinaryTreeNode<T> _leftChild;
+        BinaryTreeNode<T> _rightChild;
         List<BinaryTreeNode<T>> _children;
         
         
@@ -23,27 +23,37 @@ namespace Nojumpo.Collections
             _value = value;
             _parent = parent;
             _children = new List<BinaryTreeNode<T>>();
-            leftChild = null;
-            rightChild = null;
+            _leftChild = null;
+            _rightChild = null;
         }
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
         public bool AddChild(BinaryTreeNode<T> child, ChildSide childSide) {
-            if (leftChild == child || rightChild == child)
+            if (_leftChild == child || _rightChild == child)
             {
                 return false;
             }
 
-            if (childSide == ChildSide.LEFT && leftChild != null || 
-                childSide == ChildSide.RIGHT && rightChild != null)
+            if (childSide == ChildSide.LEFT && _leftChild != null || 
+                childSide == ChildSide.RIGHT && _rightChild != null)
             {
                 return false;
             }
 
+            if (childSide == ChildSide.LEFT)
+            {
+                _leftChild = child;
+            }
+            else
+            {
+                _rightChild = child;
+            }
+            
             child.Parent = this;
-            _children.Add(child);
             return true;
         }
+        
+        
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
 
