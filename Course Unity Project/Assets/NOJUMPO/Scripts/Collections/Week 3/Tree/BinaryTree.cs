a6646c2de93ec58758a9a24a2f838fae940a25fe
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-16 13:54:02 +0300

    Week 3: BinaryTree.cs Constructor, Fields and Properties Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTree.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTree.cs
@@ -1,18 +1,25 @@
 using System;
+using System.Collections.Generic;
 
-namespace Nojumpo
+namespace Nojumpo.Collections
 {
-    [Serializable]
-    public class BinaryTree
+    public class BinaryTree<T>
     {
         // -------------------------------- FIELDS ---------------------------------
+        public BinaryTreeNode<T> Root { get { return _root; } }
+        public int Count { get { return _nodes.Count; } }
 
+        BinaryTreeNode<T> _root = null;
+        List<BinaryTreeNode<T>> _nodes = new List<BinaryTreeNode<T>>();
 
         // ----------------------------- CONSTRUCTORS ------------------------------
-
+        public BinaryTree(T value) {
+            _root = new BinaryTreeNode<T>(value, null);
+            _nodes.Add(_root);
+        }
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
-
+        
 
         // ------------------------ CUSTOM PROTECTED METHODS -----------------------
 
