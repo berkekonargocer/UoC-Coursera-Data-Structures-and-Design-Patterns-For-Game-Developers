b09b191b6b2da1f6857f778a36dc66730673b092
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-16 17:37:20 +0300

    Week 3: BinaryTreeTest.cs Created and Implemented

--- Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeNode.cs
@@ -10,31 +10,23 @@ namespace Nojumpo.Collections
         public BinaryTreeNode<T> Parent { get { return _parent; } set { _parent = value; } }
         public BinaryTreeNode<T> LeftChild { get { return _leftChild; } }
         public BinaryTreeNode<T> RightChild { get { return _rightChild; } }
-        public IList<BinaryTreeNode<T>> Children { get { return _children.AsReadOnly(); } }
 
         T _value;
         BinaryTreeNode<T> _parent;
         BinaryTreeNode<T> _leftChild;
         BinaryTreeNode<T> _rightChild;
-        List<BinaryTreeNode<T>> _children;
 
 
         // ----------------------------- CONSTRUCTORS ------------------------------
         public BinaryTreeNode(T value, BinaryTreeNode<T> parent) {
             _value = value;
             _parent = parent;
-            _children = new List<BinaryTreeNode<T>>();
             _leftChild = null;
             _rightChild = null;
         }
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
         public bool AddChild(BinaryTreeNode<T> childToAdd, ChildSide childSide) {
-            if (_leftChild == childToAdd || _rightChild == childToAdd)
-            {
-                return false;
-            }
-
             if (childSide == ChildSide.LEFT && _leftChild != null ||
                 childSide == ChildSide.RIGHT && _rightChild != null)
             {
---
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/BinaryTreeTest.cs
@@ -0,0 +1,58 @@
+using System;
+using Nojumpo.Collections;
+using UnityEngine;
+
+namespace Nojumpo
+{
+    public class BinaryTreeTest : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+        BinaryTree<char> _binaryTree;
+
+        // ----------------------------- CONSTRUCTORS ------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+            _binaryTree = BuildBinaryTree();
+        }
+
+        void Start() {
+            Debug.Log($"{_binaryTree}");
+        }
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+
+
+        private BinaryTree<char> BuildBinaryTree() {
+            BinaryTree<char> tree = new BinaryTree<char>('A');
+            
+            BinaryTreeNode<char> nodeB = new BinaryTreeNode<char>('B', tree.Root);
+            BinaryTreeNode<char> nodeC = new BinaryTreeNode<char>('C', tree.Root);
+            BinaryTreeNode<char> nodeD = new BinaryTreeNode<char>('D', nodeB);
+            BinaryTreeNode<char> nodeE = new BinaryTreeNode<char>('E', nodeB);
+            BinaryTreeNode<char> nodeF = new BinaryTreeNode<char>('F', nodeE);
+            BinaryTreeNode<char> nodeG = new BinaryTreeNode<char>('G', nodeE);
+            BinaryTreeNode<char> nodeH = new BinaryTreeNode<char>('H', nodeC);
+            BinaryTreeNode<char> nodeI = new BinaryTreeNode<char>('I', nodeH);
+
+            tree.AddNode(nodeB, ChildSide.LEFT);
+            tree.AddNode(nodeC, ChildSide.RIGHT);
+            tree.AddNode(nodeD, ChildSide.LEFT);
+            tree.AddNode(nodeE, ChildSide.RIGHT);
+            tree.AddNode(nodeF, ChildSide.LEFT);
+            tree.AddNode(nodeG, ChildSide.RIGHT);
+            tree.AddNode(nodeH, ChildSide.RIGHT);
+            tree.AddNode(nodeI, ChildSide.LEFT);
+            
+            return tree;
+        }
+
+    }
+}
