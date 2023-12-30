2cd7f2c7780997034e315bc392f8a665fa9feef9
Berke Konargöçer <berke.konargocer@gmail.com>
2023-12-30 04:02:54 +0300

    MinimaxTreeNode.cs Created

---
+++ Course Unity Project/Assets/NOJUMPO/Scripts/Collections/Week 3/Tree/Minimax Tree/MinimaxTreeNode.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace NOJUMPO.Collections
+{
+    public class MinimaxTreeNode
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ----------------------------- CONSTRUCTORS ------------------------------
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
